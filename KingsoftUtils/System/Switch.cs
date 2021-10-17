dc7df38effaf64fc4496e830c4efca6df365fcd9
TimKral5 <TimKral5@outlook.com>
2021-10-17 12:55:53 +0200

    Projektdateien hinzufügen.

---
+++ KingsoftUtils/System/Switch.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kingsoft.Utils.System
+{
+	public class Switch
+	{
+		private List<(object, Func<object, object>)> Cases = new List<(object, Func<object, object>)>();
+		private Func<object> DefaultFunk { get; set; }
+
+		public Switch()
+		{
+			DefaultFunk = () => { return new object(); };
+		}
+
+		public Switch Case(object targObj, Func<object, object> func)
+		{
+			Cases.Add((targObj, func));
+			return this;
+		}
+
+		public Switch Default(Func<object> func)
+		{
+			DefaultFunk = func;
+			return this;
+		}
+
+		public T Run<T>(object caseInp, object funcInp)
+		{
+			foreach ((object, Func<object, object>) item in Cases)
+			{
+				if (caseInp == item.Item1)
+				{
+					return (T)item.Item2(funcInp);
+				}
+			}
+
+			if (DefaultFunk != null) return (T)DefaultFunk();
+			throw new ArgumentException();
+		}
+
+		public object Run(object caseInp, object funcInp) => Run<object>(caseInp, funcInp);
+	}
+}
